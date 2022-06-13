8bf5ccebbdebe00dcd9f2d536da3ab57b2fcd175
R-trueRoman <63845265+R-trueRoman@users.noreply.github.com>
2022-06-13 21:43:56 +0300

    001
    Ex_1

---
+++ ConsoleAppEx1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != "" || (line = Console.ReadLine()) != null)
+            {
+                line = "x: " + line;
+                line = line.Replace(",", " y:");
+                
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
